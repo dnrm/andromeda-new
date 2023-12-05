416920c89ccce62384ccf8c8db3a5cc6e59a20b4
Daniel Medina <daniel@medina.com>
2023-12-04 23:47:26 -0600

    :sparkles: Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+
+export default function Home() {
+  return (
+    <main>
+      <Navigation />
+      <Hero />
+    </main>
+  )
+}
